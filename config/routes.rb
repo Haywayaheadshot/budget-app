bfceca9b0fd68b826b875f99d7afd5ba597bcfc4
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-21 14:35:30 +0000

    Working on creating user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups, only: [:new, :show, :index, :create]
   resources :entities
-  root to: "users#new"
+  root to: "groups#index"
   resources :users, only: [:new, :index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_223439) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "photo", default: "avatar-icon.png"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
