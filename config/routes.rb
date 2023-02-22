ccb666d005abb7fed060530d4d609f6cd6bbe0e0
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-22 20:37:59 +0000

    Next devise_for routes in devise to allow creating root for unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
   resources :group_entities
-  devise_for :users
-  root to: "groups#index"
+
+    devise_for :users
+    devise_scope :user do
+      authenticated :user do
+        root 'groups#index', as: :authenticated_root
+      end
+      
+      unauthenticated :user do
+        root 'devise/sessions#welcome', as: :unauthenticated_root
+      end
+    end
+  
+
 
   resources :groups, only: [:new, :show, :index] do 
     resources :entities, only: [:index]
   end
+  
 end
