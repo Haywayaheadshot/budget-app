5d76836d7c248836ddc998fc8108cd0c4610cdba
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-02-23 23:09:35 +0000

    Fix stylelint errors

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,15 +1,15 @@
 class EntitiesController < ApplicationController
   def index
-    @group = Group.where(:user_id => current_user.id)
-    @entities = GroupEntity.where(:group_id => params[:group_id])
+    @group = Group.where(user_id: current_user.id)
+    @entities = GroupEntity.where(group_id: params[:group_id])
     @total = 0
     @entities.each do |t|
       @total += t.entity.amount
-    end 
+    end
   end
 
   def new
-    @groups = Group.where(:user_id => current_user.id)
+    @groups = Group.where(user_id: current_user.id)
     @group = Group.find(params[:group_id])
     @entity = @group.entities.new
     @group_id = params[:group_id]
@@ -18,7 +18,7 @@ class EntitiesController < ApplicationController
   def create
     @entity = Entity.new(entity_params.except(:group_id))
     @entity.user_id = current_user.id
-  
+
     if @entity.save
       entity_params[:group_id].each do |group|
         @group_entity = GroupEntity.new(group_id: group, entity_id: @entity.id)
@@ -29,11 +29,10 @@ class EntitiesController < ApplicationController
       render :new
     end
   end
-  
+
   private
-  
+
   def entity_params
     params.require(:entity).permit(:name, :amount, group_id: [])
   end
-  
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,10 +10,10 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.user_id = current_user.id
-    if @group.save
-      flash[:notice] = 'Group created successfully.'
-      redirect_to groups_path
-    end
+    return unless @group.save
+
+    flash[:notice] = 'Group created successfully.'
+    redirect_to groups_path
   end
 
   private
